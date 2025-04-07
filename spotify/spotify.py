7e61d97e959f872ae77e2f70711847758ae37ce6
unknown <wellingtonog@gmail.com>
2025-04-07 15:51:55 -0300

    change folders

--- spotify/spotify.py
+++ spotify/spotify.py
@@ -2,15 +2,13 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(layout="wide", page_title="Spotify Data")
-
-df = pd.read_csv('spotify_data.csv')
-
 st.title('Spotify Data')
 
+df = pd.read_csv('data/spotify_data.csv')
 df.set_index('Track', inplace=True)
 
 artists = df['Artist'].unique()
-selected_artist = st.selectbox('Select a Artist', artists)
+selected_artist = st.sidebar.selectbox('Select a Artist', artists)
 df_filtred = df[df['Artist'] == selected_artist]
 
 albums = df_filtred['Album'].unique()
